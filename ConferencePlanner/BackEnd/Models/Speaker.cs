50f3d2fd16a82c9a652ddcbe367f9e356a93acd3
Jon Galloway <jongalloway@gmail.com>
2017-06-12 10:09:02 +0200

    Created speaker and context

---
+++ ConferencePlanner/BackEnd/Models/Speaker.cs
@@ -0,0 +1,22 @@
+using System;
+using   System.Collections.Generic;
+using System.Linq;
+using   System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Speaker
+    {
+       public int ID { get; set; }
+
+       [Required]
+       [StringLength(200)]
+       public string Name { get; set; }
+
+       [StringLength(4000)]
+       public string Bio { get; set; }
+
+       [StringLength(1000)]
+       public virtual string WebSite { get; set; }
+    }
+}
---
+++ ConferencePlanner/BackEnd/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using BackEnd.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+             services.AddDbContext<ApplicationDbContext>(options =>
+             {
+                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                 {
+                     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                 }
+                 else
+                 {
+                     options.UseSqlite("Data Source=conferences.db");
+                 }
+             });
+             
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+        }
+    }
+}
