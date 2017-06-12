16edb6d700d585cfee5bf2ee89d260ea10790d55
Jon Galloway <jongalloway@gmail.com>
2017-06-12 11:11:47 +0200

    End of session 1

--- ConferencePlanner/BackEnd/Startup.cs
+++ ConferencePlanner/BackEnd/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BackEnd
 {
@@ -37,13 +38,27 @@ namespace BackEnd
                      options.UseSqlite("Data Source=conferences.db");
                  }
              });
-             
+
+            services.AddMvcCore()
+                .AddJsonFormatters()
+                .AddApiExplorer();
+
+            services.AddSwaggerGen(options =>
+                options.SwaggerDoc("v1", new Info { Title = "Conference Planner API", Version = "v1" })
+            );
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Conference Planner API v1")
+            );
+
             app.UseMvc();
         }
     }
